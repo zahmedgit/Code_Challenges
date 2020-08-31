38560a6b8ea7c6e554a18211f0a4c81e31d2124a
zawar <zawar.ahmed74@myhunter.cuny.edu>
2020-08-30 23:55:55 -0400

    multiply 2 nums

---
+++ Multiply_2_nums.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+"""
+
+Implement a function which multiplies two numbers.
+
+"""
+
+
+def multiply(n1,n2):
+    return n1*n2#your code here
+

53560db25a1fa0742203795a4246cf3dd2a57018
zawar <zawar.ahmed74@myhunter.cuny.edu>
2020-10-25 20:47:46 -0400

    consecutive items

---
+++ Consecutive_Items.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+"""
+
+You are given a list of unique integers arr, and two integers a and b. Your task is to find out whether or not a and b appear consecutively in arr, and return a boolean value (True if a and b are consecutive, False otherwise).
+
+It is guaranteed that a and b are both present in arr.
+
+
+
+"""
+
+
+def consecutive(arr, a, b):
+    for n in range(len(arr)-1):
+        if arr[n]==a:
+            if arr[n+1]==b:
+                return True
+        if arr[n]==b:
+            if arr[n+1]==a:
+                return True
+    return False
+    
+
